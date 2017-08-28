8a122d247a55ad67236a147b099680300e25692a
Venketesh Mahapatra <mahapatra.venketesh@gmail.com>
2017-08-29 01:58:27 +0530

    clear the graph plot again

--- datawrite.py
+++ datawrite.py
@@ -6,11 +6,12 @@ def main():
     while True:
         file = open("sampleText.txt", 'a+')
         data = time.strftime('%H:%M:%S')+" "+str(random.uniform(0.2, 0.5))+"\n"
+        # data = time.strftime('%H:%M:%S') + " " + str(int(random.uniform(0.2, 0.5))) + "\n"
         # data = time.strftime('%H:%M:%S')+" "+str( 200*math.exp(-0.05*i)*math.sin(i))+"\n"
         file.write(data)
         print(data)
         file.close() 
         i += 0.2
-        time.sleep(0.5)
+        time.sleep(0.2)
 
 main()
--- plotDistance.py
+++ plotDistance.py
@@ -6,17 +6,21 @@ fig = plt.figure()
 ax1 = fig.add_subplot(2, 2, 1)
 ax2 = fig.add_subplot(2, 2, 2)
 ax3 = fig.add_subplot(2, 1, 2)
-ax1.set_title('wire1')
-ax1.set_xlabel('time')
-ax1.set_ylabel('distance 1 ')
+
+def reset_ax1():
+    ax1.set_title('wire1')
+    ax1.set_xlabel('time')
+    ax1.set_ylabel('displacement 1 ')
+    ax1.set_xticks(range(100), minor=True)
+    # ax1.set_xticklabels(range(100))
 
 ax2.set_title('wire2')
 ax2.set_xlabel('time')
-ax2.set_ylabel('distance 2')
+ax2.set_ylabel('displacement 2')
 
 ax3.set_title('wire3')
 ax3.set_xlabel('time')
-ax3.set_ylabel('distance 3')
+ax3.set_ylabel('displacement 3')
 
 
 def animate(k):
@@ -29,19 +33,26 @@ def animate(k):
         if len(eachLine) > 1:
             x, y = eachLine.split(' ')
             if i % 3 == 0:
-                ya0.append(1+int(float(y)//0.1))
-                xa0.append(datetime.strptime(x, '%H:%M:%S'))
+                ya0.append(1+float(y)/0.1)
+                xa0.append(i)
+                # xa0.append(datetime.strptime(x, '%H:%M:%S'))
             if i % 3 == 1:
-                ya1.append(1+int(float(y)//0.1))
-                xa1.append(datetime.strptime(x, '%H:%M:%S'))
+                ya1.append(1+float(y)/0.1)
+                xa1.append(i)
+                # xa1.append(datetime.strptime(x, '%H:%M:%S'))
             if i % 3 == 2:
-                ya2.append(1+int(float(y)//0.1))
-                xa2.append(datetime.strptime(x, '%H:%M:%S'))
+                ya2.append(1+float(y)/0.1)
+                xa2.append(i)
+                # xa2.append(datetime.strptime(x, '%H:%M:%S'))
             i += 1
-    ax1.plot_date(xa0, ya0, linestyle='-', color='r')
-    ax2.plot_date(xa1, ya1, linestyle='-', color='b')
-    ax3.plot_date(xa2, ya2, linestyle='-', color='g')
+            if i%100 == 0:
+                ax1.clear()
+                reset_ax1()
+                ya0,xa0 = [],[]
+    ax1.plot(xa0, ya0, linestyle='-', color='crimson')
+    ax2.plot(xa1, ya1, linestyle='-', color='midnightblue')
+    ax3.plot(xa2, ya2, linestyle='-', color='coral')
 
 
-ani = animation.FuncAnimation(fig, animate, interval=1000)
+ani = animation.FuncAnimation(fig, animate, interval=100)
 plt.show()
