00631063e8f6d29e2804a17ac1460e7dab67889a
Venketesh Mahapatra <mahapatra.venketesh@gmail.com>
2017-09-09 23:44:05 +0530

    test.py is working sine graph generated

--- test.py
+++ test.py
@@ -1,28 +1,23 @@
 import matplotlib.pyplot as plt
 import time
+import math
 import random
 from collections import deque
-def random_gen():
-    while True:
-        val = random.randint(1,10)
-        yield val
-        time.sleep(0.1)
 
-a1 = deque([0]*100)
+a1 = deque([0]*30)
 ax = plt.axes(xlim=(0, 20), ylim=(0, 10))
-d = random_gen()
 
 line, = plt.plot(a1)
 plt.ion()
-plt.ylim([0,10])
+plt.ylim([-2,2])
 plt.show()
-
-for i in range(0,20):
-    a1.appendleft(next(d))
-    datatoplot = a1.pop()
+i = 0
+for i in range(0, 200):
+    a1.append(math.sin(i))
+    print(a1)
+    datatoplot = a1.popleft()
     line.set_ydata(a1)
     plt.draw()
-    print (a1[0])
-    i += 1
+    i += 0.2
     time.sleep(0.1)
     plt.pause(0.0001)
